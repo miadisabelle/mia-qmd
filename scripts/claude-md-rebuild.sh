09ca1d96a216cc8869a787026d2b34f8bec76bab
JGWill <jgi@jgwill.com>
2026-03-23 10:37:05 -0400

    scripts

---
+++ scripts/claude-md-rebuild.sh
@@ -0,0 +1,5 @@
+bun src/cli/qmd.ts collection remove CLAUDE-md
+bun src/cli/qmd.ts collection add /workspace --name CLAUDE-md --mask '**/CLAUDE.md'
+bun src/cli/qmd.ts embed
+
+
---
+++ scripts/rispecs-rebuild.sh
@@ -0,0 +1,4 @@
+bun src/cli/qmd.ts collection remove rispecs
+bun src/cli/qmd.ts collection add /workspace --name rispecs --mask '**/rispecs/**/*.md'
+bun src/cli/qmd.ts embed
+
