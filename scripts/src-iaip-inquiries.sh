4ddb5dabb3e08897be375b79606349a95f180efb
JGWill.As <jgi@jgwill.com>
2026-04-06 20:24:18 -0400

    fix:syntax

--- scripts/src-iaip-inquiries.sh
+++ scripts/src-iaip-inquiries.sh
@@ -8,6 +8,8 @@ QMD_MASK='*.md'
 QMD_IGNORE_PATH="/src/IAIP/prototypes/artefacts/.qmdignore"
 if [ ! -e "/src/IAIP/prototypes/artefacts/.qmdignore" ];then
     echo "__.md" 
+fi
+
 bun src/cli/qmd.ts collection remove iaip-artefacts-md 2>/dev/null || true
 bun src/cli/qmd.ts collection add $QMD_COLLECTION_PATH --name $OMD_COLLECTION_NAME --mask "'"$QMD_MASK"'"
 bun src/cli/qmd.ts embed
